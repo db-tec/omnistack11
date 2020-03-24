f57409a6cf6c5729e758499fe2e2de30a80c6fea
db-tec <eng.davidborges@gmail.com>
2020-03-23 23:55:26 -0300

    First commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 const express  = require('express'); 
+const Port = 3333;
 
 const app = express();
 
 app.get('/', (req, res) => {
     return res.json({
         Evento: "Semana OmniStack 11.0",
-        Aluno: "David Borges"
+        Aluno: "David Borges",
+        Status: "API rodando", Port
     })
 
 });
 
-app.listen(3333);
+
+app.listen(Port);
+console.log("API Rodando na Porta:", Port);
